0a3deaf9387bf18919aca0b271c541d57829aaac
Manuel <Manuel@Manuel-PC>
2016-12-04 17:47:14 +0100

    añadidas funciones dividir en tres y convertir a flotantes hecho programa principal

--- listas1/funciones/funcioneslistas.py
+++ listas1/funciones/funcioneslistas.py
@@ -6,10 +6,11 @@ def crearlista(lista):
             elemento=int(input("Introduzca los componentes de la lista"))
             lista.append(elemento)
         except:
+            print("Vuelva a introducir dicho elemento")
             elemento=input("Introduzca los componentes de la lista")
             lista.append(elemento)
-        finally:
-            x=int(input("Si desea introducir un valor pulse 1."))
+        
+        x=int(input("Si desea introducir un valor pulse 1."))
 
 "Funcion que imprime los elementos de la lista"   
 def recorrerlista(lista): 
@@ -19,3 +20,15 @@ if __name__=="__main__":
     lista=[]
     crearlista(lista)
     recorrerlista(lista)
+"Funcion que separa la lista en tres variables:id_paciente,fase_ensayo,serie_temperaturas"
+def dividirtres(lista):
+    id_paciente=lista[0:1]
+    fase_ensayo=lista[1:2]
+    serie_temperaturas=lista[2:]
+    return id_paciente,fase_ensayo,serie_temperaturas
+def convertirfloat(lista):
+    serie_temperaturas=float
+    return serie_temperaturas
+ 
+    
+    
---
+++ listas1/principal.py
@@ -0,0 +1,8 @@
+"Programa principal de las listas"
+from funciones.funcioneslistas import crearlista,convertirfloat,dividirtres
+from funciones.funcioneslistas import recorrerlista 
+lista=[]
+crearlista(lista)
+recorrerlista(lista)
+dividirtres(lista)
+convertirfloat(lista)
